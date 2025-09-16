a4c1b6bdff36aab4d9ac2a2496033cf041c2ee52
supardi98 <supardi.dev@gmail.com>
2025-09-16 21:16:52 +0700

    set user agent

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ const proxies = [
 const userAgents = [
     // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/140.0.0.0 Safari/537.36",
     // Desktop Chrome
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    // "Mozilla/5.0 (Linux; Android 15; SM-S931B Build/AP3A.240905.015.A2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36",
     // Desktop Firefox
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
-    // macOS Safari
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15",
-    // iPhone Safari
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Mobile/15E148 Safari/604.1",
-    // Android Chrome
-    "Mozilla/5.0 (Linux; Android 12; Pixel 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36",
+    // "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
+    // // macOS Safari
+    // "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15",
+    // // iPhone Safari
+    // "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Mobile/15E148 Safari/604.1",
+    // // Android Chrome
+    // "Mozilla/5.0 (Linux; Android 12; Pixel 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36",
 ];
 
 puppeteer.use(StealthPlugin());
@@ -105,8 +105,8 @@ async function initBrowser() {
             browsers.push(browser);
 
             // get user agent
-            const userAgent = await browser.userAgent();
-            console.log(`User agent: ${userAgent}`);
+            // const userAgent = await browser.userAgent();
+            // console.log(`User agent: ${userAgent}`);
 
             console.log(`Browser launched with proxy: ${proxy || "NO PROXY"}`);
         } catch (err) {
@@ -147,9 +147,11 @@ app.get("/proxy", async (req, res) => {
         page = await browser.newPage();
 
         // set user-agent random
-        // const ua = getUserAgent();
-        // await page.setUserAgent(ua);
-        // console.log(`Using User-Agent: ${ua}`);
+        if (userAgents.length > 0) {
+            const ua = getUserAgent();
+            await page.setUserAgent(ua);
+            console.log(`Using User-Agent: ${ua}`);
+        }
 
         // bikin promise untuk nunggu response
         const waitForResponse = new Promise((resolve, reject) => {
